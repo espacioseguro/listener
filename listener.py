baf53ada14e5b7d8000273519aab66d7fd3da830
Rogger Aburto <rogger.aburto@gmail.com>
2018-04-01 01:56:39 -0500

    a

--- listener.py
+++ listener.py
@@ -1,3 +1,4 @@
+#git pull https://rogger.aburto@gmail.com:Rj451300@gitlab.com/r1305/listener.git
 import socket
 import sys
 import urllib
@@ -5,7 +6,7 @@ import urllib
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
-server_address = ('localhost', 10000)
+server_address = ('localhost', 97)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 # Listen for incoming connections
