af1e2f43aae8ba99d6f62469d6e7c2356fd2718c
Rogger Aburto <rogger.aburto@gmail.com>
2018-04-01 02:11:30 -0500

    a

--- listener.py
+++ listener.py
@@ -2,6 +2,7 @@
 import socket
 import sys
 import urllib
+import requests
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,7 +23,7 @@ while True:
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(1024)
-            urllib.urlopen('https://www.espacioseguro.pe/php_connection/insertData.php?data='+data)
+            urllib.request.urlopen('https://www.espacioseguro.pe/php_connection/insertData.php?data='+data)
             print >>sys.stderr, 'received "%s"' % data
             if data:
                 print >>sys.stderr, 'sending data back to the client'
