0afe499fcb26497ee020ec49b2c7b84529dc6f4d
Rogger Aburto <rogger.aburto@gmail.com>
2018-04-07 00:25:28 -0500

    a

--- listener.py
+++ listener.py
@@ -23,7 +23,7 @@ while True:
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(1024)
-            urllib.request.urlopen('https://www.espacioseguro.pe/php_connection/insertData.php?data='+data)
+            urllib.request.urlopen('https://www.espacioseguro.pe/php_connection/47.php?data='+data)
             print >>sys.stderr, 'received "%s"' % data
             if data:
                 print >>sys.stderr, 'sending data back to the client'
