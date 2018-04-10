22eb55bb90cb5decb39f78a698deb413870588f4
Rogger Aburto <rogger.aburto@gmail.com>
2018-04-09 20:01:05 -0500

    a

--- listener.py
+++ listener.py
@@ -8,7 +8,7 @@ import urllib2
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
-server_address = ('192.168.1.100', 99)
+server_address = ('192.168.1.100', 97)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 # Listen for incoming connections
